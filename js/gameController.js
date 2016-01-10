f12538c7eb507eeb64b8e21d37abf7929c8eb084
Nathan Luo <naipton@gmail.com>
2016-01-10 15:08:58 -0500

    Added test binding

--- js/gameController.js
+++ js/gameController.js
@@ -1,3 +1,4 @@
 app.controller('gameController', ['$scope', 'boardService', function($scope, boardService){
-  $scope.board = boardService.initBoard();
+  // $scope.board = boardService.initBoard();
+  $scope.something = 5;
 }])
