ddefae08584ebe51acd212538515c0bc29d21faa
Nathan Luo <naipton@gmail.com>
2016-01-11 00:47:29 -0500

    Add key bindings to move functions

--- js/gameController.js
+++ js/gameController.js
@@ -1,4 +1,28 @@
-app.controller('gameController', ['$scope', 'boardService', function($scope, boardService){
+app.controller('gameController', ['$scope', 'boardService', 'gameService', function($scope, boardService, gameService){
   // $scope.board = boardService.initBoard();
-  $scope.something = 5;
+  $scope.gameRows = gameService.initGame();
+  $scope.newGame = function(){
+    $scope.gameRows = gameService.initGame();
+  }
+
+  Mousetrap.bind('up', function(){
+    $scope.$apply(function(){
+      $scope.gameRows = gameService.move("up");
+    });
+  });
+  Mousetrap.bind('down', function(){
+    $scope.$apply(function(){
+      $scope.gameRows = gameService.move("down");
+    })
+  });
+  Mousetrap.bind('left', function(){
+    $scope.$apply(function(){
+      $scope.gameRows = gameService.move("left");
+    });
+  });
+  Mousetrap.bind('right', function(){
+    $scope.$apply(function(){
+      $scope.gameRows = gameService.move("right");
+    });
+  });
 }])
